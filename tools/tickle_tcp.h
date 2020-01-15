a6d7dd297fb54a82dbf241857e43e93d15f8639e
Robert Altnoeder <r.altnoeder@gmx.net>
2020-01-15 15:35:31 +0100

    tickle_tcp: Added licsense notice

--- tools/tickle_tcp.h
+++ tools/tickle_tcp.h
@@ -1,3 +1,26 @@
+/**
+ * "Tickle-ACK" TCP connection failover support utility
+ *
+ * Author: Robert Altnoeder
+ * Derived from prior work authored by Jiaju Zhang, Andrew Tridgell, Ronnie Sahlberg
+ * and the Samba project.
+ *
+ * This file is part of tickle_tcp.
+ *
+ * tickle_tcp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tickle_tcp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tickle_tcp.  If not, see <https://www.gnu.org/licenses/>
+ */
+
 #ifndef TICKLE_TCP_H
 #define TICKLE_TCP_H
 
